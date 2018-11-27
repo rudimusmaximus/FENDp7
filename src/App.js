e8d5f125b94fae4cf6318150e8e9ff120b1917b1
Raul Flores, Jr. MBA "rudy <rudy@redcrowconsulting.com>
2018-11-27 03:57:14 -0600

    feat: Select categories set, todo note updates

--- src/App.js
+++ src/App.js
@@ -57,27 +57,33 @@ class App extends Component {
                           onChange="updateTipList()"
                       >
                           <option value="all">All Categories</option>
-                          <option value="cat2">Category 2</option>
-                          <option value="cat3">Category 3</option>
-                          <option value="cat4">Category 4</option>
+                          <option value="bbq">Eat BBQ</option>
+                          <option value="ent">Entertainment</option>
+                          <option value="mov">Watch a Movie</option>
+                          <option value="air">Where's The Airport</option>
                       </select>
                   </div>
                   <ul id="tip-list">Filtered list appears below. TODO: add li s
                       <ol className="tempOl">TODO:
-                          <li className="done">
-                load dfwTips data from api with error handling</li>
-                          <li className="done">paint the map</li>
+                          <li>awesome font hamburger with working toggle functionality</li>
                           <li>load the markers using json api and utility functions</li>
-                          <li>paint the search box or selectable filter</li>
                           <li>add the sidebar/menu/collapsible list</li>
                           <li>npm shorid for indexing the jsx elements</li>
                           <li>window popups for markers already done load from api</li>
-                          <li className="done">parse into components as i go</li>
-                          <li>testing</li>
                           <li>activate service worker and test</li>
-                          <li className="done">
-                bonus more info from another api like squarspace</li>
-                          <li>THORUGHOUT check the rubric checklist in issue 1</li>
+                      </ol>
+                      <p></p>
+                      <ul>
+                          <li>testing</li>
+                          <li>rubric checklist in issue 1</li>
+                      </ul>
+                      <p></p>
+                      <ol className="tempOl">DONE:
+                          <li>paint the search box or selectable filter</li>
+                          <li>paint the map</li>
+                          <li>load dfwTips data from api with error handling</li>
+                          <li>parse into components as i go</li>
+                          <li>bonus more info from another api like squarspace</li>
                       </ol>
                   </ul>
               </nav>
